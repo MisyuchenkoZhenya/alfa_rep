30b6670c825792d5a683c2ec0a1c6895448d4afa
Zhenya <misyuchenko.zhenya@mail.ru>
2017-09-11 20:06:15 +0300

    Добавьте файлы проекта.

---
+++ alfa_app/dsearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alfa_app
+{
+    /*
+     * От Вас требуется написать функцию dsearch, которая принимает на вход имя
+     * файла и блок данных. В качестве результата функция должна возвращать
+     * смещение идентичного блока данных в файле. Размер файла не превышает
+     * 512 Mb. Блоки данных в файле выровнены по границе 256 B.
+     */
+    class dsearch
+    {
+        public int dsearch_f(string file, byte block)
+        {
+
+            return 0;
+        }
+    }
+}
